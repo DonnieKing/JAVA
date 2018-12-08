80679175ebbc4b38e5615c45b4ad48d214a24060
DonnieKing <974309696@qq.com>
2018-12-08 20:41:48 +0800

    数据库连接

---
+++ 20181130_wc_buyBook/src/nuc/sw/conn/Conn.java
@@ -0,0 +1,25 @@
+package nuc.sw.conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conn {
+	public static final String DBDRIVER = "com.mysql.jdbc.Driver";
+	public static final String DBURL="jdbc:mysql://localhost:3306/javaee?useUnicode=true&characterEncoding=utf8&useSSL=true";
+	public static final String DBUSER="root";
+	public static final String DBPASS="123456";
+	Connection conn=null;
+	public Connection getConn(){
+		try {
+			Class.forName(DBDRIVER);
+			conn=DriverManager.getConnection(DBURL,DBUSER,DBPASS);
+			//System.out.println(1);
+		} 
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}

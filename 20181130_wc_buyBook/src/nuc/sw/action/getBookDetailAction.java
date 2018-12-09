c1bd265f61b9c1727b01266cbfce80a90495de11
DonnieKing <974309696@qq.com>
2018-12-09 09:11:39 +0800

    书详情页

---
+++ 20181130_wc_buyBook/src/nuc/sw/action/getBookDetailAction.java
@@ -0,0 +1,55 @@
+package nuc.sw.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import nuc.sw.dao.Dao;
+import nuc.sw.entity.Book;
+
+public class getBookDetailAction extends ActionSupport implements ModelDriven {
+
+		private Book book = new Book();
+		public Book getBook() {
+			return book;
+		}
+
+		public void setBook(Book book) {
+			this.book = book;
+		}
+		
+		List<Book> listbooks = new ArrayList<Book>();
+		public List<Book> getListbooks() {
+			return listbooks;
+		}
+
+		public void setListbooks(List<Book> listbooks) {
+			this.listbooks = listbooks;
+		}
+		private String names;		
+		public String getNames() {
+			return names;
+		}
+
+		public void setNames(String names) {
+			this.names = names;
+		}
+
+		Dao dao = new Dao();
+		
+		public String getBookDetail()
+		{
+			System.out.println(names);
+			listbooks = dao.selectABook(names);
+			return "getBookDetailOK";
+		}
+
+		@Override
+		public Object getModel() {
+			// TODO Auto-generated method stub
+			return listbooks;
+		}
+		
+}
